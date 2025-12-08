5fcccd50274b997bafff1181bf90adfde9bad0c5
BrandonVizcarr <vizcarrabrandon25@gmail.com>
2025-12-07 18:22:33 -0700

    correccion de nomobre en build

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { RouterOutlet } from '@angular/router';
   template:'<router-outlet></router-outlet>'
 })
 export class App {
-  protected readonly title = signal('front-ecomerce');
+  protected readonly title = signal('front-ecommerce');
 }
